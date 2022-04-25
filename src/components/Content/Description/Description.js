46d55199049bbd655ad297ffc58b1d8f05c44069
pingusl <pingusl@yahoo.fr>
2022-04-25 16:57:36 +0200

    Etape 2

---
+++ src/components/Content/Description/Description.js
@@ -0,0 +1,5 @@
+const Description = (props) => {
+  const description = props.text;
+  return <div className="lorem">{description}</div>;
+};
+export default Description;
---
+++ src/components/Content/Title/Title.js
@@ -0,0 +1,5 @@
+const Title = (props) => {
+  const title = props.name;
+  return <h2 className="title">{title}</h2>;
+};
+export default Title;
